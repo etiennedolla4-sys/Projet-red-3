0121b9e5e94b56d8e9f18bafcf42d63671382191
detienne <etienne.dolla@ynov.com>
2026-09-21 14:26:35 +0200

    all

--- item/potion.go
+++ item/potion.go
@@ -1,6 +1,18 @@
 package item
 
-type Potion struct {
-	Name string
-	Heal int
+import (
+	"fmt"
+
+	"Projet-red-3/characters"
+)
+
+func TakePotion(c *characters.Character) {
+	c.HP += 50
+
+	if c.HP > c.MaxHP {
+		c.HP = c.MaxHP
+	}
+
+	fmt.Println("Vous utilisez une potion de soin !")
+	fmt.Printf("PV : %d / %d\n", c.HP, c.MaxHP)
 }
