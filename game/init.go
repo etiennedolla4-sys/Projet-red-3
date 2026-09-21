aac338f1c956ce2e773eae1743f7be4ec1fdf2d2
saad ichkarran <saad.ichkarran@epita.fr>
2026-09-21 16:58:44 +0200

    a

--- game/init.go
+++ game/init.go
@@ -2,13 +2,14 @@ package game
 
 import (
 	"Projet-red-3/characters"
+	"Projet-red-3/printslow"
 	"fmt"
 )
 
 func Init() characters.Character {
 	var name string
 	var classe int
-
+	printslow.PrintSlow("Depuis des siècles, un immense gouffre appelé Veyr s'étend au cœur du royaume. Nul ne connaît sa profondeur, ni ce qui se cache au fond. Des aventuriers descendent chaque jour dans ses entrailles à la recherche de trésors, de reliques et de réponses. Mais plus ils descendent, plus le monde devient étrange… et moins ils sont nombreux à revenir.\nAujourd'hui, tu te tiens devant l'entrée de Veyr. Personne ne t'attend au fond. Personne ne sait ce que tu trouveras. \033Une seule chose est certaine : pour découvrir la vérité, tu devras descendre.\033[0m")
 	fmt.Print("Entrez votre pseudo : ")
 	fmt.Scan(&name)
 
