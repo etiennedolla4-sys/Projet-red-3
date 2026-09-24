37e5fb40ee41fb8e6d3efeaad5d4b8b27b0e1f05
saad ichkarran <saad.ichkarran@epita.fr>
2026-09-24 23:53:35 +0200

    a

--- game/combat/skills.go
+++ game/combat/skills.go
@@ -13,7 +13,7 @@ func GobelinSkill(player *characters.Character, enemy *Enemy) {
 
 	if result == 0 {
 		fmt.Println("PILE !")
-		fmt.Println("💀 Coup mortel !")
+		fmt.Println("💀 Coup Critique !")
 		enemy.HP /= 2
 	} else {
 		fmt.Println("FACE !")
